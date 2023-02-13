0e9b1ab22e2dc52fe40182e845204f3bfbdcb5b4
dorukaks <doruk@umich.edu>
2023-02-13 17:58:27 -0500

    changed import structures
    code was not working prior to these changes

--- htucker/__init__.py
+++ htucker/__init__.py
@@ -6,5 +6,7 @@ Htucker is a package for hierarchical tucker decomposition
 
 __version__ = "0.0"
 
-from htucker import algs
-from htucker.algs import *
+# from htucker import algs
+# from htucker.algs import *
+from .algs import *
+import tests
