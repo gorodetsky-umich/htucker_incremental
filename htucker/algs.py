178b0ef272f356dae4be298cd0749f05f32d6f4c
dorukaks <doruk@umich.edu>
2023-11-02 13:15:12 -0400

    (fix):change comp rat to include all training data
    previously comp rat did not include all training data
    it only included the stored snapshots in the root node
    
    this led to improper representation when append was set to False

--- htucker/algs.py
+++ htucker/algs.py
@@ -1045,7 +1045,6 @@ class HTucker:
         if append:
             self.root.core = np.concatenate((self.root.core,new_tensor),axis=-1)
             self.root.get_ranks()
-            self.batch_count+=new_tensor_shape[batch_dimension]
             return True
         else:
             return True
